9a6ed6c7c0a9b64d437063911e621c47cc42e09b
Ebenezer Tiewei <heydarkie01@gmail.com>
2025-06-01 10:44:59 +0100

    ui

--- src/components/Reusable/CopyRight.jsx
+++ src/components/Reusable/CopyRight.jsx
@@ -1,9 +1,11 @@
 import "./CopyRight.css";
 
 export default function CopyRight() {
+	let date = new Date().getFullYear();
+
 	return (
 		<div className="copyRight">
-			<p>&#xa9; 2025 Bizpadi. All Rights Reserved</p>
+			<p>&#xa9; {date} Bizpadi. All Rights Reserved</p>
 		</div>
 	);
 }
